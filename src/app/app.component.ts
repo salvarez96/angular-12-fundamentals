a94c4114ef73f391b6bdfb82656dfc39a4dbdb83
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2026-04-03 16:12:45 -0500

    Use ngFor in an easy example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name: string = '';
   age: number = 0;
+  names: string[] = ['Alice', 'Bob', 'Charlie'];
+  newName: string = '';
 
   increaseAge() {
     this.age++;
+  }
+
+  addNewName() {
+    if (this.newName.length > 0) {
+      this.names.push(this.newName);
+      this.newName = '';
+    }
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
